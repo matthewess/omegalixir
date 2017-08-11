1f27091220723a140d5fe50c385be1bc3cd092b1
Matthew Ess <mess@yelp.com>
2017-08-10 21:19:38 -0400

    Update user functionality

--- apps/omega/lib/omega/user.ex
+++ apps/omega/lib/omega/user.ex
@@ -5,22 +5,41 @@ defmodule Omega.User do
   use Agent
 
   # Public API
-  
+
   @doc """
-  Creates a user with a random name.
+  Creates a User with a random name.
   """
   def start_link(_opts) do
     name = Omega.Name.generate
-    {:ok, room} = Omega.RoomSupervisor.join(self)
-    state = %{name: name, room: room}
+    :ok = Omega.WaitingRoom.join
+    state = %{name: name, room: nil}
     Agent.start_link(fn -> state end)
   end
 
   @doc """
-  Gets the name of a given user.
+  Gets the name of a given User.
   """
   def get_name(user) do
     Agent.get(user, &Map.get(&1, :name))
   end
 
+  @doc """
+  Starts a chat with the given User in the given Room.
+  """
+  def start_chat(user, room) do
+    case get_room(user) do
+      :waiting -> Agent.update(user, &Map.update!(&1, :room, fn -> room end))
+      room -> {:error, :already_in_room}
+    end
+  end
+
+  @doc """
+  Gets the name of the Room the given User is in, or :waiting if not in a room.
+  """
+  def get_room(user) do
+    case Agent.get(user, &Map.get(&1, :room)) do
+      nil -> :waiting
+      room -> room
+    end
+  end
 end
--- apps/omega/test/omega/user_test.exs
+++ apps/omega/test/omega/user_test.exs
@@ -9,4 +9,18 @@ defmodule Omega.UserTest do
   test "name is retrievable", %{user: user} do
     assert Omega.User.get_name(user) =~ ~r|\w+\.\w+\.\d{1,4}|
   end
+
+  test "room is :waiting at start", %{user: user} do
+    assert Omega.User.get_room(user) == :waiting
+  end
+
+  test "room is updated after chat is started", %{user: user} do
+    :ok == Omega.User.start_chat(user, :dummy_room)
+    assert Omega.User.get_room(user) == :dummy_room
+  end
+
+  test "room can't be set twice", %{user: user} do
+    :ok == Omega.User.start_chat(user, :dummy_room)
+    assert Omega.User.start_chat(user, :moron_room) == {:error, :already_in_room}
+  end
 end

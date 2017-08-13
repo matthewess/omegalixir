bbec93a4efc97351a3afedec5a8268774537280e
Matthew Ess <mess@yelp.com>
2017-08-12 20:51:07 -0400

    Add waiting room test

--- apps/omega/test/omega/waiting_room_test.exs
+++ apps/omega/test/omega/waiting_room_test.exs
@@ -5,4 +5,23 @@ defmodule Omega.WaitingRoomTest do
     {:ok, room} = start_supervised Omega.WaitingRoom
     %{room: room}
   end
+
+  test "user in waiting room is in waiting state" do
+    {:ok, user} = start_supervised Omega.User
+    :ok = Omega.WaitingRoom.join(room, user)
+    assert Omega.User.get_room(user) == :waiting
+  end
+
+  test "rooms are created" do
+    range = 1..Application.fetch_env!(:omega, :max_users_per_room)
+    users = Enum.map(range, fn(_) -> 
+      {:ok, user} = start_supervised Omega.User
+      :ok = Omega.WaitingRoom.join(room, user)
+      user
+    end)
+    rooms = Enum.map(users, fn(user) ->
+      Omega.User.get_room(user)
+    end)
+    assert Set.size(Enum.uniq(rooms)) == 1
+  end
 end
